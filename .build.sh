9559ef87ba8807ec6d10409b402f7e3bcff753d8
Glen Myrland <glenmyrland@gmail.com>
2019-02-04 01:08:01 -0500

    Add robots.txt to test build in .build.sh

--- .build.sh
+++ .build.sh
@@ -1,11 +1,15 @@
+rm -r public/
+#aws s3 rm s3://hg.gmyrland.com-test/ --recursive
+
 sudo docker build . -t hg.gmyrland.com
 sudo docker run --rm \
     -v "$(pwd)":/home/site \
     --name hg.gmyrland.com-build \
     hg.gmyrland.com hugo
 sudo chown -R glen:glen public/
-aws s3 sync public/ s3://hg.gmyrland.com-test/ --delete # test
-#aws s3 sync public/ s3://hg.gmyrland.com/ --delete # prod
 
-# delete test bucket contents
-#aws s3 rm s3://hg.gmyrland.com-test/ --recursive
+#aws s3 sync public/ s3://hg.gmyrland.com-test/ --size-only --delete # test
+aws s3 sync public/ s3://hg.gmyrland.com/ --delete # prod
+
+# add robots.txt to test
+aws s3 cp layouts/robots.txt s3://hg.gmyrland.com-test/robots.txt
